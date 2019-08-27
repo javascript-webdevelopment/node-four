4726896b163fb79150a128f1d06bba7f18c988f5
tstokes9328 <taytestokes9328@gmail.com>
2019-08-26 18:39:42 -0600

    finished mwf

--- index.js
+++ index.js
@@ -8,17 +8,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Custom TLM
-app.use((req, res, next) => {
-    console.log('custom tlm hit!')
-    // invoke next to move onto the next funciton in the req
-    next();
+// Dummy Data
+let user = {
+    username: 'tayte123',
+    password: 'password123',
+    full_name: 'Tayte Stokes',
+    color: 'black'
+};
+
+// Custom Middleware Functions
+const authenticateUser = (req, res, next) => {
+    const {username, password} = req.body;
+    if(username === user.username && password === user.password){
+        // invoke next and pass in the user
+        next()
+    } else {
+        res.status(403).send('Invalid username or password');
+    }
+};
+
+// End Point to login
+app.post('/api/login', authenticateUser, (req, res) => {
+    // send back user object
+    res.status(200).send(user);
 });
-
-
-
-// EndPoints
-app.get('/api', (req, res) => res.send('Hello'))
 
 // App Listening
 app.listen(8080, () => {
